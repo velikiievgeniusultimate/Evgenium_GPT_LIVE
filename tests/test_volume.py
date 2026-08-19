e0dfb1a75c849b2b7bf2a0654b4a237f6af28f6b
velikiievgeniusultimate <velikiievgeniusultimate@gmail.com>
2026-08-19 04:47:11 +0300

    Test EGL volume targeting by private Xvfb display

--- tests/test_volume.py
+++ tests/test_volume.py
@@ -5,15 +5,34 @@ import egl.volume as volume
 
 
 class VolumeTests(unittest.TestCase):
-    def test_only_egl_stream_is_changed_and_volume_is_clamped(self):
+    def test_gui_autodiscovery_changes_only_private_egl_chromium(self):
         streams = [
             {
                 "index": 17,
-                "properties": {"application.name": volume.EGL_AUDIO_APP_NAME},
+                "properties": {
+                    "application.name": "WEBRTC VoiceEngine",
+                    "application.process.binary": "Discord",
+                    "media.role": "phone",
+                    "window.x11.display": ":1",
+                },
             },
             {
                 "index": 18,
-                "properties": {"application.name": "Chromium"},
+                "properties": {
+                    "application.name": "Chromium",
+                    "application.process.binary": "chromium",
+                    "media.role": "phone",
+                    "window.x11.display": ":90",
+                },
+            },
+            {
+                "index": 19,
+                "properties": {
+                    "application.name": "Chromium",
+                    "application.process.binary": "chromium",
+                    "media.role": "phone",
+                    "window.x11.display": ":1",
+                },
             },
         ]
         calls = []
@@ -35,7 +54,48 @@ class VolumeTests(unittest.TestCase):
         self.assertEqual(changed, 1)
         self.assertEqual(
             calls,
-            [["/usr/bin/pactl", "set-sink-input-volume", "17", "150%"]],
+            [["/usr/bin/pactl", "set-sink-input-volume", "18", "150%"]],
+        )
+
+    def test_daemon_exact_display_targets_only_that_display(self):
+        streams = [
+            {
+                "index": 21,
+                "properties": {
+                    "application.process.binary": "chromium",
+                    "media.role": "phone",
+                    "window.x11.display": ":90",
+                },
+            },
+            {
+                "index": 22,
+                "properties": {
+                    "application.process.binary": "chromium",
+                    "media.role": "phone",
+                    "window.x11.display": ":91",
+                },
+            },
+        ]
+        calls = []
+
+        class Result:
+            returncode = 0
+
+        def fake_run(args, **kwargs):
+            calls.append(args)
+            return Result()
+
+        with (
+            patch.object(volume, "_pactl", return_value="/usr/bin/pactl"),
+            patch.object(volume, "_sink_inputs", return_value=streams),
+            patch.object(volume.subprocess, "run", side_effect=fake_run),
+        ):
+            changed = volume.set_assistant_volume(120, x11_display=":91")
+
+        self.assertEqual(changed, 1)
+        self.assertEqual(
+            calls,
+            [["/usr/bin/pactl", "set-sink-input-volume", "22", "120%"]],
         )
 
     def test_no_pactl_is_non_blocking(self):
